bc69338d6807c4daf3d9173e36a9255496561134
Nnaji Chimuanya <nnajichimuanyua50@gmail.com>
2022-09-15 13:03:30 -0700

    Set up the folder and neccesary subfolders

--- user microservice/src/index.ts
+++ user microservice/src/index.ts
@@ -1,4 +1,6 @@
 import express, { Request, Response, Express } from "express";
+import dotenv from "dotenv";
+dotenv.config();
 const app: Express = express();
 
 app.get("/", (req: Request, res: Response) => {
