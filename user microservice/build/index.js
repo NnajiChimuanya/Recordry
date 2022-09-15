3e8d71440303287396f1f58218bb4befcaaf839b
Nnaji Chimuanya <nnajichimuanyua50@gmail.com>
2022-09-15 12:59:25 -0700

    Initialized this sproject

--- user microservice/build/index.js
+++ user microservice/build/index.js
@@ -1,2 +1,12 @@
 "use strict";
-console.log("Hello");
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const app = (0, express_1.default)();
+app.get("/", (req, res) => {
+    console.log("Recordry user microservice");
+    res.send("Recordry user microservice");
+});
+app.listen(3000, () => console.log("Hello"));
--- user microservice/src/index.ts
+++ user microservice/src/index.ts
@@ -1,1 +1,9 @@
-console.log("Hello");
+import express, { Request, Response, Express } from "express";
+const app: Express = express();
+
+app.get("/", (req: Request, res: Response) => {
+  console.log("Recordry user microservice");
+  res.send("Recordry user microservice");
+});
+
+app.listen(3000, () => console.log("Hello"));
